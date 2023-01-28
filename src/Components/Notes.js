6b46258c1f3a9fa17ba6c9e029f020aeecef3acb
Utkarsh <dhavaleutkarsh@gmail.com>
2023-01-29 00:37:02 +0530

    Added Notes and Notetem components

---
+++ src/Components/Notes.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import context from '../Context-Notes/noteContext';
+import NoteItem from './NoteItem';
+
+
+function Notes() {
+    const { notes, setnotes } = useContext(context);
+    return (
+        <div className="row my-3">
+            <h3>Your Notes</h3>
+            {notes.map((note) => {
+                return <NoteItem note={note}/>
+            })}
+        </div>
+    )
+}
+
+export default Notes
--- src/Context-Notes/NoteState.js
+++ src/Context-Notes/NoteState.js
@@ -1,9 +1,68 @@
+import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState =(props)=>{
+   const notesArr = [
+    {
+      "_id": "63bc5a3479f71c60b2aaee52",
+      "user": "63ba7c0f3ccc3c9b03805a72",
+      "title": "Set Alarm",
+      "description": "Wake up at 6 AM",
+      "tag": "Alarm",
+      "date": "2023-01-09T18:17:24.784Z",
+      "__v": 0
+    },
+    {
+        "_id": "63bc5a3479f71c60b2aaee52",
+        "user": "63ba7c0f3ccc3c9b03805a72",
+        "title": "Set Alarm",
+        "description": "Wake up at 6 AM",
+        "tag": "Alarm",
+        "date": "2023-01-09T18:17:24.784Z",
+        "__v": 0
+    },
+    {
+        "_id": "63bc5a3479f71c60b2aaee52",
+        "user": "63ba7c0f3ccc3c9b03805a72",
+        "title": "Set Alarm",
+        "description": "Wake up at 6 AM",
+        "tag": "Alarm",
+        "date": "2023-01-09T18:17:24.784Z",
+        "__v": 0
+    },
+    {
+        "_id": "63bc5a3479f71c60b2aaee52",
+        "user": "63ba7c0f3ccc3c9b03805a72",
+        "title": "Set Alarm",
+        "description": "Wake up at 6 AM",
+        "tag": "Alarm",
+        "date": "2023-01-09T18:17:24.784Z",
+        "__v": 0
+    },
+    {
+        "_id": "63bc5a3479f71c60b2aaee52",
+        "user": "63ba7c0f3ccc3c9b03805a72",
+        "title": "Set Alarm",
+        "description": "Wake up at 6 AM",
+        "tag": "Alarm",
+        "date": "2023-01-09T18:17:24.784Z",
+        "__v": 0
+    },
+    {
+        "_id": "63bc5a3479f71c60b2aaee52",
+        "user": "63ba7c0f3ccc3c9b03805a72",
+        "title": "Set Alarm",
+        "description": "Wake up at 6 AM",
+        "tag": "Alarm",
+        "date": "2023-01-09T18:17:24.784Z",
+        "__v": 0
+    }
+  ]
+
+  const [notes, setNotes] = useState(notesArr);
 
    return(
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes,setNotes}}>
             {props.children}
         </NoteContext.Provider>
    );
